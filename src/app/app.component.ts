df5f4acb4c96b66411911429b657625c05cd9f8a
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-04-07 16:32:07 +0500

    letscode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomeComponent } from './componentes/pages/home/home.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'Letscode';
+  constructor(private primengConfig: PrimeNGConfig) {}
+  ngOnInit(): void {
+    initFlowbite();
+    this.primengConfig.ripple = true;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/pages/home/home.component';
+import { ProblemsetComponent } from './componentes/pages/problemset/problemset.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{path:"", title:'Home', component:HomeComponent},
+{path:"home", title:'Home', component:HomeComponent},
+{path:"problemsset", title:'Problem', component:ProblemsetComponent},
+];
